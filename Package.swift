d44ed514d17d48dd15c9ed5583fd8bd9e2bb8979
Tim <0xtimc@gmail.com>
2019-12-19 09:10:55 +0000

    Update to Swift 5 and update README for GH actions

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
+// swift-tools-version:5.1
+
 import PackageDescription
 
 let package = Package(
-    name: "cmark"
+    name: "cmark",
+    products: [
+        .library(name: "cmark", targets: ["cmark"]),
+    ],
+    targets: [
+        .target(name: "cmark")
+    ]
 )
